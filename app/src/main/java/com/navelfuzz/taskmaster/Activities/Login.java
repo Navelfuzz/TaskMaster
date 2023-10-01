152acf14b58e267b27dcec1c9f2d17f320b49c8f
Jonathon Stillson <jcstillson77@gmail.com>
2023-10-01 14:58:04 -0400

    built out remaining frameworks

--- app/src/main/java/com/navelfuzz/taskmaster/Activities/Login.java
+++ app/src/main/java/com/navelfuzz/taskmaster/Activities/Login.java
@@ -1,2 +1,45 @@
-package com.navelfuzz.taskmaster.activities;public class Login {
+package com.navelfuzz.taskmaster.activities;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.navelfuzz.taskmaster.MainActivity;
+import com.navelfuzz.taskmaster.R;
+import com.amplifyframework.core.Amplify;
+
+public class Login extends AppCompatActivity {
+    public static String TAG = "LoginActivity";
+
+    Button submitButton;
+    EditText email;
+    EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        submitButton = findViewById(R.id.LoginActivitySubmitButton);
+        email = findViewById(R.id.LoginActivityEmailInput);
+        password = findViewById(R.id.LoginActivityPasswordInput);
+
+        setupSubmitButton();
+    }
+
+    void setupSubmitButton(){
+        submitButton.setOnClickListener(view -> {
+            Amplify.Auth.signIn(email.getText().toString(),password.getText().toString(),
+                    success -> {
+                        Log.i(TAG, "Sign in succeeded.");
+                        Intent goToMainActivity = new Intent(Login.this, MainActivity.class);
+                        startActivity(goToMainActivity);
+                    },
+                    failure -> Log.e(TAG, "Sign in failed."));
+        });
+
+    }
 }
--- app/src/main/java/com/navelfuzz/taskmaster/Activities/Verification.java
+++ app/src/main/java/com/navelfuzz/taskmaster/Activities/Verification.java
@@ -1,2 +1,61 @@
-package com.navelfuzz.taskmaster.activities;public class Verification {
+package com.navelfuzz.taskmaster.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.navelfuzz.taskmaster.R;
+import com.amplifyframework.core.Amplify;
+
+public class Verification extends AppCompatActivity {
+    public static String TAG = "VerificationActivity";
+
+    Button submitButton;
+    EditText email;
+    EditText confirmationCode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification);
+
+        submitButton = findViewById(R.id.VerifyActivitySubmitButton);
+        email = findViewById(R.id.VerifyActivityEmailInput);
+        confirmationCode = findViewById(R.id.VerifyActivityCodeInput);
+
+        setupEmailTextInput();
+
+        setupSubmitButton();
+
+
+    }
+
+    void setupEmailTextInput(){
+        Intent callingIntent = getIntent();
+        String emailString = null;
+
+        if(callingIntent != null) {
+            emailString = callingIntent.getStringExtra("email");
+        }
+
+        if (emailString != null){
+            email.setText(emailString);
+        }
+    }
+
+    void setupSubmitButton() {
+        Amplify.Auth.confirmSignUp(email.getText().toString(),
+                confirmationCode.getText().toString(),
+                successResponse -> {
+                    Log.i(TAG, "Verification Succeeded.");
+                    Intent goToLoginIntent = new Intent(Verification.this, Login.class);
+                    goToLoginIntent.putExtra("email", email.getText().toString());
+                    startActivity(goToLoginIntent);
+                },
+                failure -> Log.e(TAG, "Verification Failed"));
+    }
 }
